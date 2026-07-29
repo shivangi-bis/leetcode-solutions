2f8e2b176a8b6cdca08b91ece50c47637709787f
shivangi-bis <shivangibis1406@gmail.com>
2026-07-30 01:00:29 +0530

    Time: 0 ms (100%), Space: 12.3 MB (88.7%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        result = 0
+
+        for ch in columnTitle:
+            value = ord(ch) - ord('A') + 1
+            result = result * 26 + value  
+
+        return result 
+        
